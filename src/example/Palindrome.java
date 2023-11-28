3e546c21dff9256e4e27091faaf158a54ab9974e
poornima <poornimapoorni724@gmail.com>
2023-11-28 23:19:51 +0530

    Java program to find palindrome of two number

---
+++ src/example/Palindrome.java
@@ -0,0 +1,28 @@
+package example;
+
+public class Palindrome {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		String str="madam";
+		String rev="";
+		for(int i=str.length()-1;i>=0;i--)
+		{
+			rev=rev+str.charAt(i);
+		}
+		if (str.equals(rev))
+		{
+			System.out.println("palindrome");
+			
+		}
+		else
+		{
+			System.out.println("not pal");
+		}
+
+	}
+
+}
+
+
+	
